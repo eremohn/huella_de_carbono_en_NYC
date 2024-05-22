2eeed808e21a795bae324f1d216fe348f94050a2
Javier <7.matute@gmail.com>
2024-05-22 01:37:41 -0500

    App_33%

--- Sprint_3/App/app.py
+++ Sprint_3/App/app.py
@@ -3,7 +3,8 @@ import geopandas as gpd
 import streamlit as st
 import folium
 from streamlit_folium import st_folium
-from branca.colormap import linear
+import branca.colormap as cm
+
 #____________________ Page configuration
 
 
@@ -14,66 +15,98 @@ st.set_page_config(
 
 #_____________________ Source dataset reading.
 
+g_data = r'Sprint_3\App\Data\Borough_Boundaries.geojson'
+data = (r'Sprint_3\App\Data\yellow_taxis.csv')
 
-data = r"D:\0_Respaldo\0_Proyectos_2024\Henry-PF\PF\huella_de_carbono_en_NYC\Sprint_3\App\Data\Borough Boundaries.geojson"
-data1 = r"Sprint_3\App\Data\yellow_taxis.csv"
-df = gpd.read_file(data)
-df1 = pd.read_csv(data1)
+@st.cache_data   #cache the csv file
+def load_data(csv_file_path):
+    df = pd.read_csv(csv_file_path)
+    return df
+
+@st.cache_data   #cache the csv file
+def load_geoData(geo_file_path):
+    df = gpd.read_file(geo_file_path)
+    return df
+
+
+data = load_data(data)
+gdf = load_geoData(g_data)
+
+#______________________________________Sidebar
+
+with st.sidebar:
+    st.title('Analisis')
+    '---'
+    selection = st.radio('Seleciona el analisis',['Cantidad de viajes','Total ganancias', 'Promedio del pasaje', 'Total distancia recorrida'], index=0,)
+    
+    if selection == 'Cantidad de viajes':
+        selection = 'cant_registros'
+    elif selection == 'Total ganancias':
+        selection = 'costo_total'
+    elif selection == 'Promedio del pasaje':
+        selection = 'costo_promedio'
+    elif selection == 'Total distancia recorrida':
+        selection = 'distancia_promedio'
+
+
+
+
+
+
+#______________________Dataframe
+
+df = gdf[['boro_name', 'geometry']]
+df_cp = data[['boro_name',selection]]
+
+#_____________________Merge
+
+df_geo = df.merge(df_cp, on='boro_name')
+
 
 #________________________Glabal Variables
+#Color variable
+myscale = (df_geo[selection].quantile((0,0.25,0.5,0.75,0.85,1))).tolist()
 
 
-
-
-
-
-#________________________Colormap
-
-colormap = linear.YlGn_09.scale(
-    df1.cant_registros.min(), df1.cant_registros.max()
-)
-
-#________________________Global Variables
-color_dict = df1.set_index("boro_name")["cant_registros"]
 
 #________________________Body
 
-
 st.subheader('Ciudad de Nueva York')
-m = folium.Map(location=[40.71483, -73.99726], 
-            tiles='Esri_WorldGrayCanvas', 
-            zoom_control=False, 
-            zoom_start=10, 
-            dragging=False,
-            min_zoom=10,
-            scrollWheelZoom=False,
-            max_bounds=True,               
-            )
+m = folium.Map(location=[40.71483, -73.99726],
+                tiles='Esri_WorldGrayCanvas', 
+                zoom_start=10,
+                zoom_control=False,
+                dragging=False,
+                scrollWheelZoom=False,
+                max_bounds=True,)
+
+#folium.TileLayer('CartoDB positron',name="Light Map",control=False).add_to(m)
+
+# choropleth = folium.Choropleth(
+#     geo_data=g_data
+# )
+
+choropleth = folium.Choropleth(
+    geo_data=g_data,
+    name='Choropleth',
+    legend_name='Cantidad de viajes',
+    data=df_geo,
+    columns=['boro_name',selection],
+    key_on="feature.properties.boro_name",
+    fill_color='YlGnBu',   #YlGnBu
+    threshold_scale=myscale,
+    fill_opacity=.5,
+    line_opacity=.75,
+    line_weight=2,
+    line_color='darkgray',    
+    smooth_factor=0  
+)
+choropleth.geojson.add_to(m)
 
 
 
-folium.GeoJson(df,
-    name="total_viajes",
-    style_function=lambda feature: {
-        "fillColor": colormap(color_dict.get(feature["properties"]["boro_name"], 0)),
-        "color": "black",
-        "weight": 1,
-        "dashArray": "5, 5",
-        "fillOpacity": 0.5,
-        
-    },
-).add_to(m)
 
 
+st_folium(m, width=500, height=700)
 
-
-
-
-
-
-colormap.caption = "Total de viajes"
-colormap.add_to(m)
-
-
-st_data = st_folium(m, width=600, height=700)
 
